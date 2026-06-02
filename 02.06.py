bfb0d725192bedca52ea34f534e9cc93c0bf06c2
Взрослая академия <georgygevorkov@gmail.com>
2026-06-02 21:07:51 +0300

    условия

---
+++ 02.06.py
@@ -0,0 +1,32 @@
+# num1 = float(input('введите число :'))
+# d1 = num1%10
+# d2 = num1//10%10
+# d3 = num1//100%10
+# d4 = num1//1000
+# print((d1*1000+d2*100+d3*10+d4)*2)
+# я
+#
+#
+
+# if num1>0:
+#     print(">0")
+#     print('конец условия')
+# print('конец условия')
+#
+# num1 = int(input())
+# num2 = int(input())
+# if num1>num2:
+#     print(f'{num1}')
+# elif num1==num2:
+#     print('они равны')
+# else:
+#     print(f'{num2}')
+name=input()
+# if name == 'vanya' or 'Vanya':
+#     print('yes')
+# else:
+#     print('no')
+
+
+
+
