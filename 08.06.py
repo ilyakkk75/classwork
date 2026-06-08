774a31d39843d81380e9b1e58e8824736a3d5684
Взрослая академия <georgygevorkov@gmail.com>
2026-06-08 21:09:00 +0300

    while and more

---
+++ 08.06.py
@@ -0,0 +1,96 @@
+# n1 = float(input())
+# n2 = float(input())
+# n3 = float(input())
+# if n1>n2 and n1>n3:
+#     print(f'{n1}')
+# elif n2>n1 and n2>n3:
+#     print(f'{n2}')
+# else:
+#     print(f'{n3}')
+
+# n1=float(input())
+# if n1%2==0:
+#     print('четное')
+# else:
+#     print('нечетное')
+
+
+
+# n1=int(input())
+# if n1>10 and n1<=20:
+#     print('yes')
+# else:
+#     print('no')
+
+
+# n1 = int(input())
+# n2=int(input())
+# if (n1%3==0) and (n2%3==0):
+#     print('true')
+# else:
+#     print('false')
+
+
+
+# while n>=0 :
+#     print(n, end= ' ')
+#     n=n-1
+# a=int(input())
+# b=int(input())
+# if a<b:
+#     a,b=b,a
+# while a>=b and a!=1:
+#     print(a, end=' ')
+#     a=a+1
+#
+# a=int(input())
+#
+# if a<b:
+#     a,b=b,a
+# while a>=b:
+#     if b%10==3:
+#         print(b, end=" ")
+#     b=b+1
+n=int(input())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
