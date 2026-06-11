bc89308bbcd7728427a94cba8dc15c3b8059e6ca
Взрослая академия <georgygevorkov@gmail.com>
2026-06-11 19:22:32 +0300

    while and more

---
+++ 4.06.py
@@ -0,0 +1,50 @@
+# a = int(input())
+# b = int(input())
+# if a>b:
+#     print(a)
+# elif a==b:
+#     print(f'{a} = {b}')
+# else :
+#     print(b)
+#
+# km=float(input())
+# time=float(input())
+# print(km/time)
+# '''пользователь вводит три числа, выести большее из этих чисел'''
+# num1 = float(input())
+# num2 = float(input())
+# num3 = float(input())
+# if num1>num2 :
+#     if num1>num3 :
+#         print(num1)
+#     else :
+#         print(num3)
+# elif num2>num3 :
+#         print(num2)
+# else :
+#     print(num3)
+'''решение 1'''
+# n1 = int(input())
+# if (n1 < 10 and n1 >= 0) or n1 == 10 :
+#     print(f'{n1}')
+# else :
+#     print('error')
+
+'''решение 2'''
+# if n1<10 :
+#     print(f'{n1}')
+# elif n1 == 10 :
+#     print(f'{n1}')
+# else:
+#     print('error')
+'''решение 3'''
+
+# if n1<10 or n1==10 :
+#     print(f'{n1}')
+# else :
+#     print('error')
+
+
+
+
+
