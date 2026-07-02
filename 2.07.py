c3d0727b1d31a6653af9235e564ce88826039659
Взрослая академия <georgygevorkov@gmail.com>
2026-07-02 21:09:53 +0300

    while and more

---
+++ 2.07.py
@@ -0,0 +1,43 @@
+# # 1
+import random
+# list1 = [0,0,0,0,0,0,0,0,0,0]
+# a=int(input())
+# b=int(input())
+# for i in range(len(list1)):
+#     list1[i]=random.randint(a,b)
+# print(list1)
+# # 2
+# max=list1[0]
+# for i in range(len(list1)):
+#     if list1[i]>max:
+#         max=list1[i]
+# print(max)
+# # 3\
+# n=0
+# list2 = [0,0,0,0,0,0,0,0,0,0]
+# for i in range(len(list1)):
+#     if list1[i]%2==0:
+#         list2[n]=list1[i]
+#         n+=1
+# for i in range(len(list1)):
+#     if list1[i]%2!=0:
+#         list2[n]=list1[i]
+#         n+=1
+# print(list2)
+
+# def show_list():
+#     print('-начало программы-')
+# show_list()
+# print('№1' )
+
+# def maximum(list1):
+#     max=list1[0]
+#     for i in range (len(list1)):
+#         if list1[i]>max:
+#             max=list1[i]
+#     return max
+# school=[[2,3,5,2,5,4,6,3],
+#         [2,4,5,3],
+#         [2,5,4,4,4,4,4]]
+# print(maximum(school))
+
--- 29.06.py
+++ 29.06.py
@@ -7,22 +7,22 @@
 # marks[1],marks[len(marks)-2]=marks[len(marks)-2],marks[1]
 # marks[2],marks[len(marks)-3]=marks[len(marks)-3],marks[2]
 # print(marks)
-a=[[2,3,4],[1,8,6]]
-for i in range(len(a)):
-    summa=0
-    for j in a[i]:
-        summa+=j
-        print(j,end='\t')
-    print('|',summa)
-print('----------------')
-result=0
-for i in range(len(a[0])):
-    summa=0
-    for j in range(len(a)):
-        summa+=a
-    print(summa,end='\t')
-    result+=summa
-print('|',result)
+# a=[[2,3,4],[1,8,6]]
+# for i in range(len(a)):
+#     summa=0
+#     for j in a[i]:
+#         summa+=j
+#         print(j,end='\t')
+#     print('|',summa)
+# print('----------------')
+# result=0
+# for i in range(len(a[0])):
+#     summa=0
+#     for j in range(len(a)):
+#         summa+=a
+#     print(summa,end='\t')
+#     result+=summa
+# print('|',result)
 
 
 
