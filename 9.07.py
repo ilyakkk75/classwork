d53c970bd94be7490c34b3581b9fb2e2ecf8e843
Взрослая академия <georgygevorkov@gmail.com>
2026-07-09 21:01:58 +0300

    while and more

---
+++ 9.07.py
@@ -0,0 +1,38 @@
+# st = ['калабухов', 'вячаславович', 'олеговна']
+# for i in st:
+#     # if i[-2:] == 'на':
+#         print(i)
+
+# st=['сергей','алексей','евгений','кот']
+# glas='аеёиоуыэюя'
+# for i in st:
+#     if i[0] in glas:
+#             print(i)
+# st=('whefthwothgosuih begin antihypeozamaykpssoxxxy and sjushsi')
+# def ret(st):
+#     return st[st.find('begin')+5:st.find('end')]
+# print(ret(st))
+# st='qw kodw dwdd qw'
+# print(st.split(' ')) делает из предложения список
+# print(st.replace('qw','!')) заменит qw на !
+# st='если сделал уроки и помыл посуду можешь идти гулять!'
+# lt=st.split(' ')
+# st2=''
+# for i in lt:
+#     if i[-1]=='л':
+#         i+='а'
+#     st2+=i+' '
+# print(st2)
+
+# st='апельсин мандарин апельсин тоже фрукт что и мандарин'
+# ls=st.split(' ')
+# lt=set(ls)
+# lt2=list(lt)
+# print(lt2)
+
+st='4 4 3 53 3 5'
+cif=0
+for i in st:
+    if i.isdigit():
+        cif+=1
+print(cif)
